75e8aa3dcd0717954724a70d2eb308706798f91e
janisbeikm <jaanis.beikmanis@inbox.lv>
2025-04-10 23:55:34 +0300

    Update __init__.py

--- components/pcm60x/__init__.py
+++ components/pcm60x/__init__.py
@@ -1,6 +1,2 @@
 # Marker file for PCM60X component
-import esphome.codegen as cg
-from esphome.components import seplos_modbus
-import esphome.config_validation as cv
-from esphome.const import CONF_ID
 from . import pcm60x
